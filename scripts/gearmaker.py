52bebec6e2ffe48461b86c5246432586d2e41f48
Revenge-of-Shadow <vyacheslav.pigida@gmail.com>
2025-05-17 21:41:19 +0200

    0E: Added tooth angle modifier and gear editing functionality.

--- scripts/gearmaker.py
+++ scripts/gearmaker.py
@@ -2,142 +2,38 @@ from PySide import QtCore, QtGui
 import FreeCAD as App
 import math
 
-'''============================================================='''
-'''                Graphical user interface code                '''
-class GuiClass(QtGui.QDialog):
-    def __init__(self):
-        super(GuiClass, self).__init__()
-        self.initUI()
-
-    ##  "Error handling"
-    def callInformation(self):
-        QtGui.QMessageBox.information(None, "Nya nya",
-                                      "Outer radius must be bigger than inner radius and tooth height together."+
-                                      "\nZero inner radius is allowed."+
-                                      "\nAmount of teeth must be above 2."+
-                                      "\nAmount and outer radius influence tooth width."+
-                                      "\nConnected gearwheels must have the same tooth width and the same tooth height."+
-                                      "\nExtrusion may be positive, negative (reversed) and nil (sketch only)."
-                                      )
-
-    ##  "Error handling" end
-
-    ## Utility functions
-    def setupSpinBox(self, box, max, min = 0, step = 1, default = 0):
-        box.setRange(min, max)
-        box.setSingleStep(step)
-        box.setValue(default)
-
-    def tryQuit(self, success):
-        if(success):
-            if(self.ds_or.value() <= (self.ds_th.value()+self.ds_ir.value())):
-                self.callInformation()
-                return
-        self.success = success
-        self.close()
-    ## Utility functions end
-    
-    ##  Event handling
-    def onValueChanged(self):
-        #   outer radius <= tooth height + inner radius
-        if(self.ds_or.value() <= (self.ds_th.value()+self.ds_ir.value())):
-            self.is_tw.setValue(0)
-        else:
-            self.is_tw.setValue(math.tan(math.pi*2/self.is_at.value()/2)*(self.ds_or.value()-self.ds_th.value())*2)
-            self.is_td.setValue(math.tan(math.pi*2/self.is_at.value()/2)*(self.ds_or.value())*2)
-
-    def onOk(self):
-        self.tryQuit(True)
-
-    def onCancel(self):
-        self.tryQuit(False)
-   ##  Event handling end
-    
-
-    def initUI(self):
-        self.success = False
-        self.setGeometry(250, 250, 320, 400)
-        self.setFixedSize(320, 480)
-        self.setWindowTitle("Nya")
-
-        ##  Labels and inputs
-        self.l_or = QtGui.QLabel("Outer radius [mm]:", self)
-        self.l_or.move(20, 20)
-        self.ds_or = QtGui.QDoubleSpinBox(self)
-        self.setupSpinBox(box=self.ds_or, max=1000000, min=1, step = 1, default=5)
-        self.ds_or.valueChanged[float].connect(self.onValueChanged)
-        self.ds_or.setFixedWidth(80)
-        self.ds_or.move(220, 20)
-
-        self.l_ir = QtGui.QLabel("Inner radius [mm]:", self)
-        self.l_ir.move(20, 70)
-        self.ds_ir = QtGui.QDoubleSpinBox(self)
-        self.setupSpinBox(box=self.ds_ir, max=1000000, min=0, step=1, default=1) 
-        self.ds_ir.valueChanged[float].connect(self.onValueChanged)
-        self.ds_ir.setFixedWidth(80)
-        self.ds_ir.move(220, 70)
-
-        self.l_at = QtGui.QLabel("Amount of teeth:", self)
-        self.l_at.move(20, 120)
-        self.is_at = QtGui.QSpinBox(self)
-        self.setupSpinBox(box=self.is_at, max = 1000, min = 2, step = 1, default = 16) 
-        self.is_at.valueChanged[int].connect(self.onValueChanged)
-        self.is_at.setFixedWidth(80)
-        self.is_at.move(220, 120)
-
-        self.l_th = QtGui.QLabel("Tooth height [mm]:", self)
-        self.l_th.move(20, 170)
-        self.ds_th = QtGui.QDoubleSpinBox(self)
-        self.setupSpinBox(box=self.ds_th, max=1000, min=0, step=0.1, default=1)
-        self.ds_th.valueChanged[float].connect(self.onValueChanged)
-        self.ds_th.setFixedWidth(80)
-        self.ds_th.move(220, 170)
-
-        self.l_e = QtGui.QLabel("Extrusion [mm]:", self)
-        self.l_e.move(20, 220)
-        self.ds_e = QtGui.QDoubleSpinBox(self)
-        self.setupSpinBox(self.ds_e, 1000000, min=-1000000, step=1, default=2) 
-        self.ds_e.valueChanged[float].connect(self.onValueChanged)
-        self.ds_e.setFixedWidth(80)
-        self.ds_e.move(220, 220)
-
-        self.l_tw = QtGui.QLabel("Tooth width [mm]:", self)
-        self.l_tw.move(20, 270)
-        self.is_tw = QtGui.QDoubleSpinBox(self)
-        self.is_tw.setFixedWidth(80)
-        self.is_tw.move(220, 270)
-        self.is_tw.setEnabled(False)
-
-        self.l_td = QtGui.QLabel("Distance between peaks [mm]:", self)
-        self.l_td.move(20, 320)
-        self.is_td = QtGui.QDoubleSpinBox(self)
-        self.is_td.setFixedWidth(80)
-        self.is_td.move(220, 320)
-        self.is_td.setEnabled(False)
-        ##  Inputs and labels end
-
-        ##  Confirm/Cancel buttons
-        self.bt_cancel = QtGui.QPushButton("Cancel", self)
-        self.bt_cancel.clicked.connect(self.onCancel)
-        self.bt_cancel.move(20, 440)
-        
-        self.bt_ok = QtGui.QPushButton("Make the gear", self)
-        self.bt_ok.clicked.connect(self.onOk)
-        self.bt_ok.move(190, 440)
-        ##  Confirm/Cancel buttons end
-        
-        self.show()
-
-form = GuiClass()
-form.exec()
-
-'''               Graphical user interface end                '''
-'''==========================================================='''
-
 '''==========================================================='''
 '''                     Modelling code                        '''
+sketch = None
+body = None
+pad = None
+doc = App.activeDocument()
+if(doc is None):
+    QtGui.QMessageBox.information(None, "No nya", "Select a document first.")
+
+
 ##  FreeCAD object functions
-def makeGearWheel(doc, o_radius, i_radius, tooth_amount, tooth_h, extrusion, angle_divider = 2):
+def removePrev():
+    global body 
+    global sketch 
+    global pad 
+
+    if(pad is not None):
+        doc.removeObject(pad.Name)
+        pad = None
+    if(sketch is not None):
+        doc.removeObject(sketch.Name)
+        sketch = None
+    if(body is not None):
+        doc.removeObject(body.Name)
+        body = None
+
+def makeSketch(o_radius, i_radius, tooth_amount, tooth_h, angle_divider):
+    removePrev()
+
+    global body 
+    global sketch 
+   
     body = doc.addObject('PartDesign::Body', 'gearBody')
 
     sketch = body.newObject('Sketcher::SketchObject', 'gearSketch')
@@ -183,9 +79,17 @@ def makeGearWheel(doc, o_radius, i_radius, tooth_amount, tooth_h, extrusion, ang
     geoList.append(Part.LineSegment(points[0], points[len(points)-1]))
     sketch.addGeometry(geoList, False)
     del geoList
+    
+    doc.recompute()
 
+    return sketch
+
+def makeGearWheel(extrusion):
     if(extrusion == 0):
         return
+
+    global pad
+    
 
     pad = body.newObject('PartDesign::Pad', 'gearPad')
     pad.Profile = (sketch, [''])
@@ -194,21 +98,154 @@ def makeGearWheel(doc, o_radius, i_radius, tooth_amount, tooth_h, extrusion, ang
     pad.Reversed = (extrusion < 0)
 
     sketch.Visibility = False
+    doc.recompute()
 ##  FreeCAD object functions end
 
-if(form.success):
-    doc = App.activeDocument()
-    if(doc is None):
-        QtGui.QMessageBox.information(None, "No nya", "Select a document first.")
-    else:
-        ##  Processing
-        o_radius = form.ds_or.value()
-        i_radius = form.ds_ir.value()
-        tooth_amount = form.is_at.value()
-        tooth_h = form.ds_th.value()
-        extrusion = form.ds_e.value()
-        
-        makeGearWheel(doc, o_radius, i_radius, tooth_amount, tooth_h, extrusion)
+'''                     Modelling code end                      '''
+'''============================================================='''
 
-        ##  Processing end
-        doc.recompute()
+'''============================================================='''
+'''                Graphical user interface code                '''
+class GuiClass(QtGui.QDialog):
+    def __init__(self):
+        super(GuiClass, self).__init__()
+        self.initUI()
+
+    ##  "Error handling"
+    def callInformation(self):
+        QtGui.QMessageBox.information(None, "Nya nya",
+                                      "Outer radius must be bigger than inner radius and tooth height together."+
+                                      "\nZero inner radius is allowed."+
+                                      "\nAmount of teeth must be above 2."+
+                                      "\nAmount and outer radius influence tooth width."+
+                                      "\nConnected gearwheels must have the same tooth width and the same tooth height."+
+                                      "\nExtrusion may be positive, negative (reversed) and nil (sketch only)."
+                                      )
+
+    ##  "Error handling" end
+
+    ## Utility functions
+    def setupSpinBox(self, box, max, min = 0, step = 1, default = 0):
+        box.setRange(min, max)
+        box.setSingleStep(step)
+        box.setValue(default)
+
+    def areValuesBad(self):
+        return self.ds_or.value() <= (self.ds_th.value()+self.ds_ir.value())
+
+   
+    ## Utility functions end
+    
+    ##  Event handling
+    def onValueChanged(self):
+        #   outer radius <= tooth height + inner radius
+        if(self.ds_or.value() <= (self.ds_th.value()+self.ds_ir.value())):
+            self.is_tw.setValue(0)
+        else:
+            self.is_tw.setValue(math.tan(math.pi*2/self.is_at.value()/2)*(self.ds_or.value()-self.ds_th.value())*2)
+            self.is_td.setValue(math.tan(math.pi*2/self.is_at.value()/2)*(self.ds_or.value())*2)
+
+    def onOk(self):
+        if(self.areValuesBad()):
+            self.callInformation()
+            return
+
+        makeSketch(self.ds_or.value(), 
+                       self.ds_ir.value(), 
+                       self.is_at.value(), 
+                       self.ds_th.value(), 
+                       self.ds_ao.value())
+        makeGearWheel(self.ds_e.value())
+
+    def onCancel(self):
+        self.close()
+   ##  Event handling end
+    
+
+    def initUI(self):
+        self.setGeometry(250, 250, 320, 400)
+        self.setFixedSize(320, 480)
+        self.setWindowTitle("Nya")
+
+        ##  Labels and inputs
+        self.l_or = QtGui.QLabel("Outer radius [mm]:", self)
+        self.l_or.move(20, 20)
+        self.ds_or = QtGui.QDoubleSpinBox(self)
+        self.setupSpinBox(box=self.ds_or, max=1000000, min=1, step = 1, default=5)
+        self.ds_or.valueChanged[float].connect(self.onValueChanged)
+        self.ds_or.setFixedWidth(80)
+        self.ds_or.move(220, 20)
+
+        self.l_ir = QtGui.QLabel("Inner radius [mm]:", self)
+        self.l_ir.move(20, 70)
+        self.ds_ir = QtGui.QDoubleSpinBox(self)
+        self.setupSpinBox(box=self.ds_ir, max=1000000, min=0, step=1, default=1) 
+        self.ds_ir.valueChanged[float].connect(self.onValueChanged)
+        self.ds_ir.setFixedWidth(80)
+        self.ds_ir.move(220, 70)
+
+        self.l_at = QtGui.QLabel("Amount of teeth:", self)
+        self.l_at.move(20, 120)
+        self.is_at = QtGui.QSpinBox(self)
+        self.setupSpinBox(box=self.is_at, max = 1000, min = 2, step = 1, default = 16) 
+        self.is_at.valueChanged[int].connect(self.onValueChanged)
+        self.is_at.setFixedWidth(80)
+        self.is_at.move(220, 120)
+
+        self.l_th = QtGui.QLabel("Tooth height [mm]:", self)
+        self.l_th.move(20, 170)
+        self.ds_th = QtGui.QDoubleSpinBox(self)
+        self.setupSpinBox(box=self.ds_th, max=1000, min=0, step=0.1, default=1)
+        self.ds_th.valueChanged[float].connect(self.onValueChanged)
+        self.ds_th.setFixedWidth(80)
+        self.ds_th.move(220, 170)
+
+        self.l_e = QtGui.QLabel("Extrusion [mm]:", self)
+        self.l_e.move(20, 220)
+        self.ds_e = QtGui.QDoubleSpinBox(self)
+        self.setupSpinBox(self.ds_e, 1000000, min=-1000000, step=1, default=2) 
+        self.ds_e.valueChanged[float].connect(self.onValueChanged)
+        self.ds_e.setFixedWidth(80)
+        self.ds_e.move(220, 220)
+
+        self.l_ao = QtGui.QLabel("Angle offset modifier:", self)
+        self.l_ao.move(20, 270)
+        self.ds_ao = QtGui.QDoubleSpinBox(self)
+        self.setupSpinBox(self.ds_ao, 1000000, min=1, step=1, default=1) 
+        self.ds_ao.valueChanged[float].connect(self.onValueChanged)
+        self.ds_ao.setFixedWidth(80)
+        self.ds_ao.move(220, 270)
+        
+        self.l_tw = QtGui.QLabel("Tooth width [mm]:", self)
+        self.l_tw.move(20, 320)
+        self.is_tw = QtGui.QDoubleSpinBox(self)
+        self.is_tw.setFixedWidth(80)
+        self.is_tw.move(220, 320)
+        self.is_tw.setEnabled(False)
+
+        self.l_td = QtGui.QLabel("Distance between peaks [mm]:", self)
+        self.l_td.move(20, 370)
+        self.is_td = QtGui.QDoubleSpinBox(self)
+        self.is_td.setFixedWidth(80)
+        self.is_td.move(220, 370)
+        self.is_td.setEnabled(False)
+        ##  Inputs and labels end
+        ##  Confirm/Cancel buttons
+        self.bt_ok = QtGui.QPushButton("Make the gear", self)
+        self.bt_ok.clicked.connect(self.onOk)
+        self.bt_ok.move(190, 440)
+
+        self.bt_cancel = QtGui.QPushButton("Quit", self)
+        self.bt_cancel.clicked.connect(self.onCancel)
+        self.bt_cancel.move(20, 440)
+        ##  Confirm/Cancel buttons end
+        
+        self.show()
+
+form = GuiClass()
+form.exec()
+
+'''               Graphical user interface end                '''
+'''==========================================================='''
+
+
