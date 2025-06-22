fbba8900ede18af2821c7804ed8a4c44d3308aa6
Revenge-of-Shadow <vyacheslav.pigida@gmail.com>
2025-06-22 19:52:04 +0200

    29: Mushroom screw algorithm created.

--- scripts/screwmaker.py
+++ scripts/screwmaker.py
@@ -1,3 +1,4 @@
+import math
 '''==================================================================================='''
 '''                                Modelling functions                                '''
 '''==================================================================================='''
@@ -50,15 +51,31 @@ def makeSubtractiveHelix(body, length, diameter, wire_diameter, pitch):
     return helix
 
 
-def makeHeadConeSketch(body, diameter, head_diameter, height):
+def makeHeadConeSketch(body, diameter, bigger_diameter, height):
     sketch_head = body.newObject('Sketcher::SketchObject','HeadSketch')
     sketch_head.AttachmentSupport = (doc.getObject('YZ_Plane'),[''])
     sketch_head.MapMode = 'FlatFace'
 
     sketch_head.addGeometry(Part.LineSegment(App.Vector(0, 0, 0), App.Vector(diameter/2, 0, 0)), False)
-    sketch_head.addGeometry(Part.LineSegment(App.Vector(diameter/2, 0, 0), App.Vector(head_diameter/2, -height, 0)), False)
-    sketch_head.addGeometry(Part.LineSegment(App.Vector(head_diameter/2, -height, 0), App.Vector(0, -height, 0)), False)
+    sketch_head.addGeometry(Part.LineSegment(App.Vector(diameter/2, 0, 0), App.Vector(bigger_diameter/2, -height, 0)), False)
+    sketch_head.addGeometry(Part.LineSegment(App.Vector(bigger_diameter/2, -height, 0), App.Vector(0, -height, 0)), False)
     sketch_head.addGeometry(Part.LineSegment(App.Vector(0, -height, 0), App.Vector(0, 0, 0)), False)
+    sketch_head.Visibility = False
+
+    return sketch_head
+
+
+def makeHeadShroomSketch(body, diameter, height, curve_radius):
+    sketch_head = body.newObject('Sketcher::SketchObject', 'HeadSketch')
+    sketch_head.AttachmentSupport = (doc.getObject('YZ_Plane'),[''])
+    sketch_head.MapMode = 'FlatFace'
+
+    end_angle = -math.acos(diameter/2/curve_radius)
+    end_y     = curve_radius + curve_radius*math.sin(end_angle)-height
+    sketch_head.addGeometry(Part.LineSegment(App.Vector(0, 0, 0), App.Vector(diameter/2, 0, 0)), False)
+    sketch_head.addGeometry(Part.LineSegment(App.Vector(diameter/2, 0, 0), App.Vector(diameter/2, end_y, 0)), False)
+    sketch_head.addGeometry(Part.ArcOfCircle(Part.Circle(App.Vector(0, curve_radius-height, 0), App.Vector(0, 0, 1), curve_radius), -math.pi/2, end_angle))
+    sketch_head.addGeometry(Part.LineSegment(App.Vector(0, 0, 0), App.Vector(0, -height, 0)), False)
     sketch_head.Visibility = False
 
     return sketch_head
@@ -87,7 +104,7 @@ diameter = 3
 length = 10
 pitch = wire_diameter + wire_diameter/2
 head_height = 2
-head_diameter = 4
+head_diameter = 5
 
 
 body = None
@@ -99,5 +116,6 @@ else:
 
 makeCylinderPad(body, diameter, length)
 makeSubtractiveHelix(body, length, diameter, wire_diameter, pitch)
-revolveSketchZ(body, makeHeadConeSketch(body, diameter, head_diameter, head_height))
+#revolveSketchZ(body, makeHeadConeSketch(body, diameter, head_diameter, head_height))
+revolveSketchZ(body, makeHeadShroomSketch(body, head_diameter, head_height, length/2))
 doc.recompute()
